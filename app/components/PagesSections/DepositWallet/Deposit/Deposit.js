be3ffbde5dbf05035afe7f099744d6fbdc35749f
Anthony Synenko <anthony.synenko@gmail.com>
2018-04-24 18:46:17 +0300

    WIZ-39 deposit page

--- app/components/PagesSections/DepositWallet/Deposit/Deposit.js
+++ app/components/PagesSections/DepositWallet/Deposit/Deposit.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Deposit.css';
 
+import Input from '../../../UI/Input/Input';
+
 const deposit = props => (
   <div className={classes.Deposit}>
-    Deposit
+    <div className={classes.Top}>
+      <h3>
+        Deposit
+      </h3>
+      <Input
+        type={props.depositPlanSelect.type}
+        elementType={props.depositPlanSelect.elementType}
+        value={props.depositPlanSelect.value}
+        changed={val => props.handleDepositPlanChange(val)}
+        elementConfig={props.depositPlanSelect.elementConfig}
+      />
+    </div>
+    <div className={classes.Middle}>
+      <input type="text" value="0xa658b225a2c34579963612eea3d61c7755ebf8c6" />
+      <button>Copy</button>
+      <button>Qr-code</button>
+    </div>
+    <div className={classes.Bottom}>
+      <h3>
+        Important
+      </h3>
+      <p>
+        Send only ETH to this deposit address.Sending any other
+        currency to this address may result in the loss of
+        your deposit.
+      </p>
+    </div>
   </div>
 );
+
+deposit.propTypes = {
+  depositPlanSelect: PropTypes.shape().isRequired,
+  handleDepositPlanChange: PropTypes.func.isRequired
+};
 
 export default deposit;
--- app/components/PagesSections/DepositWallet/DepositWallet.js
+++ app/components/PagesSections/DepositWallet/DepositWallet.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './DepositWallet.css';
 
@@ -7,9 +8,22 @@ import RateCalc from './RateCalc/RateCalc';
 
 const depositWallet = props => (
   <div className={classes.DepositWallet}>
-    <Deposit />
-    <RateCalc />
+    <Deposit
+      depositPlanSelect={props.depositPlanSelect}
+      handleDepositPlanChange={val => props.handleDepositPlanChange(val)}
+    />
+    <RateCalc
+      calculator={props.calculator}
+      handleCalculatorFieldChange={(val, key) => props.handleCalculatorFieldChange(val, key)}
+    />
   </div>
 );
+
+depositWallet.propTypes = {
+  depositPlanSelect: PropTypes.shape().isRequired,
+  handleDepositPlanChange: PropTypes.func.isRequired,
+  calculator: PropTypes.shape().isRequired,
+  handleCalculatorFieldChange: PropTypes.func.isRequired
+};
 
 export default depositWallet;
