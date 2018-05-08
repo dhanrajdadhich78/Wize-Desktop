2092715d5425e99385e2f0fe9625a65ae18e525f
Dhanraj Dadhich <dhanraj.dadhich78@gmail.com>
2018-05-08 18:19:01 +0300

    WIZ-42 exclude auth

--- app/utils/const.js
+++ app/utils/const.js
@@ -3,9 +3,11 @@ export const DIGEST_URL = 'http://localhost:8888';
 export const BLOCKCHAIN_URL = 'http://localhost:4000';
 
 // export const DIGEST_URL = 'http://master.wizeprotocol.com:8888';
+// export const BLOCKCHAIN_URL = 'http://master.wizeprotocol.com:4000';
+
 // export const RAFT_URL = 'http://localhost:11001/key';
 // export const FS_URL = 'http://localhost:13000/buckets';
-// export const BLOCKCHAIN_URL = 'http://master.wizeprotocol.com:4000';
+
 // common
 export const MONTH_ARRAY = [
   'January',
